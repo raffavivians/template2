58940e3888b20cfda29510207c97f2564f924944
raffavivians <raffavivians@gmail.com>
2020-12-27 19:02:06 +0100

    Primo commit

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Immagine from "../image/immagine1.jpg"
+
+const Home = () => {
+
+  return (
+   <div>
+     <h1>PROVIAMO</h1>
+     {/* <Immagine ></Immagine> */}
+  </div>
+  )
+}
+
+export default Home
